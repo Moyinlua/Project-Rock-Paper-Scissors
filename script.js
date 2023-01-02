52f6310a134c4980497bc84f2fa80a2b176b705a
Moyinoluwa <moyinoluwa001@gmail.com>
2023-01-02 16:13:50 +0100

    add eventListeners and textcontent

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let choices = ['Rock', 'Paper', 'Scissors'];
 
-
 function getComputerChoice (){
     i = Math.floor(Math.random()*choices.length);
     let computerSelection = choices[i];
-    return computerSelection.toLowerCase();
+    return computerSelection = computerSelection.toLowerCase();
 }
-//let comSelect = getComputerChoice()
-//let computerSelection = comSelect.toLowerCase()
 
-//console.log(getComputerChoice())
 let losePbR = 'You Lose! Paper beats Rock'
 let winPbR = 'You Win! Paper beats Rock'
 let loseSbP = 'You Lose! Scissors beat Paper'
@@ -33,48 +29,124 @@ function playRound(playerSelection, computerSelection){
     }else { return "it's a tie"}
 }
 
+const body = document.getElementById('mainBody');
 
+const choiceList = document.createElement('div');
+body.appendChild(choiceList);
 
-//console.log(playRound(playerSelection, computerSelection))
-//console.log(`Computer selects ${comSelect}`)
+const btnRock = document.createElement('button');
+btnRock.appendChild(document.createTextNode('ROCK'));
+btnRock.setAttribute('class', 'userChoice');
 
-function game(){
-    let count = [];
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('enter Rock, Paper or Scissors: ', '').toLowerCase();
-        let computerSelection;
-        computerSelection
-        count[i] = playRound(playerSelection,getComputerChoice());
-        
-        
+const btnPaper = document.createElement('button');
+btnPaper.appendChild(document.createTextNode('PAPER'));
+btnPaper.setAttribute('class', 'userChoice');
+
+const btnScissors = document.createElement('button');
+btnScissors.appendChild(document.createTextNode('SCISSORS'));
+btnScissors.setAttribute('class', 'userChoice')
+
+let aR = [];
+
+let outcomeAnyAtCount5 = document.createElement('h3');
+outcomeAnyAtCount5.id = 'first-to-5';
+
+let scoreSummary = document.createElement('h4');
+
+const report = document.createElement('div');
+
+function scoreDisplay (){
+
+    //const report = document.createElement('div');
+    report.textContent = result;
+    report.className = 'report'
+    console.log(result, 'bululluu')
+    body.appendChild(report);
+
+    var scoreList = document.getElementsByClassName('report')
+
+    //console.log(scoreList)
+
+    //console.log(Array.from(scoreList));
+
+    function scoreArray(){
+        let len = Array.from(scoreList).length - 1;
+        aR.push(Array.from(scoreList)[len].textContent);
+        return aR;
     }
-    return count;
-}
+    console.log(scoreArray());
 
-console.log(fiveRoundGame = game())
+    let winCount = aR.filter(x => x=='player wins').length;
+    let lossCount = aR.filter(x => x=='comp wins').length;
+    console.log(winCount, lossCount, 'bubu');
 
+    //let scoreSummary = document.createElement('h4');
+    scoreSummary.textContent =`Player ${winCount} | ${lossCount} Computer`;
+    body.appendChild(scoreSummary);
 
-function scoreBoard(){
-    let scoreBoard = [];
-    for (let i = 0; i < fiveRoundGame.length; i++){
-        if ((fiveRoundGame[i] === losePbR) || (fiveRoundGame[i] === loseRbS) || (fiveRoundGame[i] === loseSbP)) { 
-            scoreBoard[i] = 'lose';
-        } else if ((fiveRoundGame[i] === winPbR) || (fiveRoundGame[i] === winRbS) || (fiveRoundGame[i] === winSbP)) {
-            scoreBoard[i] = 'win';
-        } else {scoreBoard[i] = 'tie';}                   
+    var verdict;
+    function outcome5 () {
+        if ((winCount == 5) && (lossCount < 5)){
+            verdict = "PLAYER WINS";
+        }
+        else if ((winCount < 5) && (lossCount == 5)){
+            verdict = "COMPUTER WINS";
+        } else {
+            verdict = '';
+        }
+        return verdict;
     }
     
-    return scoreBoard;
+    //console.log(outcome5(), 'bulaba');
+    outcomeAnyAtCount5.textContent = outcome5();
+
+    body.appendChild(outcomeAnyAtCount5);
 }
 
-console.log(scoreBoard());
+btnPaper.addEventListener('click', function () {
+    let playerSelection = "paper";
+    let computerSelection = getComputerChoice ();
+    if ((playerSelection) && (computerSelection === 'paper')){
+       result = 'tie'};
+    if ((playerSelection) && (computerSelection === 'scissors')) {
+       result = 'comp wins'};       //loss, scissors cut paper
+    if ((playerSelection) && (computerSelection === 'rock')) {
+       result = 'player wins'};     //win, paper wraps rock   
+   
+    scoreDisplay ();
 
-let winCount = scoreBoard().filter(x => x=='win').length
-let loseCount = scoreBoard().filter(x => x=='lose').length
+})
 
-if (winCount > loseCount){
-    console.log('Victory');
-}else if (winCount < loseCount){
-    console.log('Better luck next time');
-}else console.log('A Tie');
+btnRock.addEventListener('click', function () {
+    let playerSelection = "rock";
+    let computerSelection = getComputerChoice ();
+    if ((playerSelection) && (computerSelection === 'paper')){
+       result = 'comp wins'};       //loss, paper wraps rock
+    if ((playerSelection) && (computerSelection === 'scissors')) {
+       result = 'player wins'};     //win, rock crushes scissors
+    if ((playerSelection) && (computerSelection === 'rock')) {
+       result = 'tie'};   
+    
+    scoreDisplay ();
+})
+
+btnScissors.addEventListener('click', function () {
+    let playerSelection = "scissors";
+    let computerSelection = getComputerChoice ();
+    if ((playerSelection) && (computerSelection === 'paper')){
+       result = 'player wins'};      //win, scissors cuts paper
+    if ((playerSelection) && (computerSelection === 'scissors')) {
+       result = 'tie'};
+    if ((playerSelection) && (computerSelection === 'rock')) {
+       result = 'comp wins'};        //loss, rock crushes scissors  
+    
+    scoreDisplay ();
+})
+
+choiceList.appendChild(btnRock);
+
+choiceList.appendChild(btnPaper);
+
+choiceList.appendChild(btnScissors);
+
 
